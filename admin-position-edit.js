98b32f9659cb5679005529341fed2d82e0b17609
amine daudo <adaddasmaravilhas@gmail.com>
2026-08-26 00:34:55 +0200

    Carregar tabela detalhada de impressão

--- admin-position-edit.js
+++ admin-position-edit.js
@@ -1,3 +1,4 @@
 // Edição de vagas. O login administrativo é tratado apenas pelo app-core.js.
 // Mantemos um único fluxo de autenticação para evitar dois handlers concorrentes.
 await import('./admin-position-edit-core.js?v=20260826-0001');
+await import('./print-report-table.js?v=20260826-0140');
